7b060d7f8ecb8ba700dc3bf189b96515f44797f0
Jacob Albright <jacob.albright23@gmail.com>
2020-05-24 11:28:09 -0500

    WIP - fetching data from 'GET /api/address/info'. displaying raw json response

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,18 @@
 <template>
   <section class="container-fluid">
     <h1>Hello</h1>
-    <TheAddressSearchbar label="Search by address:" v-model="addrSearch" />
+    <TheAddressSearchbar label="Search by address:" @submit="onSubmit" v-model="addrSearch" />
+    <ul>
+      <li v-for="(x, i) in searchResults" :key="i">
+        <pre>{{x}}</pre>
+      </li>
+    </ul>
   </section>
 </template>
 
 <script>
+import * as api from '@/services/api'
+
 import TheAddressSearchbar from '@/components/address-searchbar'
 export default {
   components: {
@@ -15,7 +22,18 @@ export default {
     return {
       selected: '',
       addrSearch: '',
+      searchResults: [],
     }
   },
+  methods: {
+    onSubmit() {
+      api.fetchAddressInfo({ q: this.addrSearch }).then(res => {
+        this.searchResults = res.data
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    }
+  }
 }
 </script>

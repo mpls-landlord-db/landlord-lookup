ee0f56da2ae7dcdab8f49118feb3669383cdc598
Jacob Albright <32335624+Jaco26@users.noreply.github.com>
2020-07-18 12:38:04 -0500

    Feature/navbar (#22)
    * WIP - have working responsive navbar component
    
    * WIP - added links and link styling

---
+++ components/the-navbar.vue
@@ -0,0 +1,55 @@
+<template>
+  <nav class="navbar">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col offset-md-1 offset-0 col-12 col-md-10">
+          <nav class="navbar__content">
+            <section class="title">
+              <nuxt-link exact class="navlink" to="/">Landlord Lookup</nuxt-link>
+            </section>
+            <section class="links">
+              <nuxt-link class="navlink" exact to="/">Home</nuxt-link>
+              <nuxt-link class="navlink" exact to="/about">About</nuxt-link>
+            </section>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.navbar {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  background: aliceblue;
+  box-shadow: 1px 1px 2px #aaa;
+  &__content {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .title {
+      .navlink {
+        font-size: 1.5rem;
+        font-weight: 700;
+        text-decoration: none;
+        color: black;
+      }
+    }
+    .links {
+      .navlink {
+        margin-left: .5rem;
+        font-size: 1.125rem;
+      }
+    }
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,91 @@
 <template>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col offset-md-1 offset-0 col-12 col-md-10">
-        <nuxt />
+  <div class="layout" :class="{ 'layout--is-scrolled' : scrollY > 40 }">
+
+    <div class="navbar-wrapper">
+      <TheNavbar />
+    </div>
+
+    <div class="main-content-wrapper">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col offset-md-1 offset-0 col-12 col-md-10">
+            <nuxt />
+          </div>
+        </div>
       </div>
     </div>
 
   </div>
 </template>
 
-<style>
+<script>
+import TheNavbar from '@/components/the-navbar'
+export default {
+  components: {
+    TheNavbar,
+  },
+  data() {
+    return {
+      scrollY: 0,
+    }
+  },
+  mounted() {
+    this.syncScrollY()
+    window.addEventListener('scroll', this.syncScrollY)
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.syncScrollY)
+  },
+  methods: {
+    syncScrollY() {
+      this.scrollY = window.scrollY
+    }
+  },
+}
+</script>
 
+<style lang="scss">
+
+$navbar-height: 64px;
+$navbar-height-scrolled: 32px;
+
+.layout {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+
+  .navbar-wrapper {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 2000;
+    width: 100%;
+    margin: 0;
+    height: $navbar-height;
+    transition: all .1s ease-in-out;
+  }
+
+  .main-content-wrapper {
+    background: #fafafa;
+    margin-top: $navbar-height;
+    width: 100%;
+    min-height: calc(100% - #{$navbar-height});
+    transition: all .1s ease-in-out;
+  }
+
+  &--is-scrolled {
+
+    .navbar-wrapper {
+      height: $navbar-height-scrolled;
+    }
+
+    .main-content-wrapper {
+      margin-top: $navbar-height-scrolled;
+      min-height: calc(100% - #{$navbar-height-scrolled});
+    }
+
+  }
+}
 </style>

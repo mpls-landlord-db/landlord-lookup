d8bd8371017b5c133077fb6757a611de42189c80
Jacob Albright <jacob.albright23@gmail.com>
2020-07-18 21:21:31 -0500

    added more content to /about page

--- pages/about.vue
+++ pages/about.vue
@@ -4,7 +4,7 @@
       <div class="col p-0">
         <article class="elevator-pitch">
           <section class="elevator-pitch__content">
-            We empower tenants in Minneaplois to find current rental license information for the homes 
+            We empower tenants in Minneapolis to find current rental license information for the homes 
             in which they live as well as find out which other properties in Minneapolis share the
             same owner/landlord/management company.
           </section>
@@ -20,18 +20,19 @@
               The data comes from a spreadsheet of active rental license data made public by the city of Minneapolis. You can find it
               <a class="link" href="https://opendata.minneapolismn.gov/datasets/active-rental-licenses/data" target="_blank">here</a>.
             </faq-item>
-            <faq-item q='In the search results list, what does "match confidence" mean?'>
-              Our search bar works in two steps:
-              <ul class="dashed">
-                <li>
-                  First, we look for any database entries with an address that matches the one entered into it. 
-                </li>
-                <li>
-                  Each entry includes over 30 pieces of information including the property owner's:
-                  name, mailing address, phone number, and email address. If we find an entry whose property address
-                  matches the one you put into the search bar, we use those 4 peices of information ... WIP
-                </li>
-              </ul>
+            <faq-item q='In the search results list, what does "Match Confidence" mean?'>
+              <p>
+                The short answer: the more green boxes in the "match confidence" column, the more sure we are that this address is 
+                owned and/or managed by the same person and/or company as the address you put into the search bar.
+              </p>
+              <div>
+                The long answer: <button class="expandy-wandy" @click="showMore = !showMore">[show {{showMore ? 'less -' : 'more +'}}]</button>
+              </div>
+
+              <div v-if="showMore" class="the-long-answer">
+                We're working on it... It's not too complicated, but successfully composing an engaging, and understandable piece of  
+                technical writing is hard.
+              </div>
             </faq-item>
           </ol>
         </article>
@@ -48,17 +49,7 @@ export default {
   },
   data() {
     return {
-      faqs: [
-        {
-          q: 'Where does the data come from?',
-          a: `The data comes from a spreadsheet made public by the city of Minneapolis. 
-          You can find it <a class="link" href="https://opendata.minneapolismn.gov/datasets/active-rental-licenses/data" target="_blank">here</a>.`
-        },
-        {
-          q: 'In the search results list, what does "match confidence" mean?',
-          a: 'When we search'
-        }
-      ]
+      showMore: false,
     }
   }
 }
@@ -93,10 +84,25 @@ export default {
   }
 }
 
-.dashed {
+.listy-wisty {
   list-style: inside;
   li {
     margin: 8px 0;
   }
+}
+.expandy-wandy {
+  cursor: pointer;
+  background: transparent;
+  padding: 4px;
+  outline: none;
+  border: 1px solid transparent;
+  &:hover {
+    background: #eee;
+  }
+}
+.the-long-answer {
+  background: #eee;
+  margin-top: .25rem;
+  padding: 1rem;
 }
 </style>

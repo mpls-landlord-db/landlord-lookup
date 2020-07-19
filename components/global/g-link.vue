5b63b25d598ed36bf44ceeff3cebb8ba9aca3641
Jacob Albright <32335624+Jaco26@users.noreply.github.com>
2020-07-18 21:26:14 -0500

    Develop (#25)
    * Feature/navbar (#22)
    
    * WIP - have working responsive navbar component
    
    * WIP - added links and link styling
    
    * added a new g-link component to make links focusable on firefox (#23)
    
    * added a new g-link component to make links focusable on firefox
    
    * exposed nuxt-link's 'exact' prop to GLink props api
    
    * Feature/about page (#24)
    
    * WIP - did a lot of work on /about page content and layout. Refactored /home page layout and content for NO_SEARCH_RESULTS_STATE
    
    * added more content to /about page

---
+++ components/global/g-link.vue
@@ -0,0 +1,47 @@
+<template functional>
+  <nuxt-link
+    tabindex="-1"
+    role="link"
+    :exact="props.exact"
+    :to="props.to"
+    :class="[data.staticClass, data.class]"
+  >
+    <button class="g-link">
+      <slot></slot>
+    </button>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  props: {
+    to: [String, Object],
+    exact: Boolean,
+  }
+}
+</script>
+
+<style lang="scss" >
+.g-link {
+  font: inherit;
+  padding: 4px;
+  outline: none;
+  background: transparent;
+  border: 1px solid transparent;
+  border-radius: 2px;
+  cursor: pointer;
+  &:focus,
+  &:hover {
+    background: #0001;
+  }
+}
+
+.nuxt-link-active,
+.nuxt-link-exact-active {
+  .g-link {
+    text-decoration-line: underline;
+    text-decoration-thickness: 2px;
+    text-decoration-color: #03a59d;
+  }
+}
+</style>
---
+++ components/the-navbar.vue
@@ -0,0 +1,54 @@
+<template>
+  <nav class="navbar">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col offset-md-1 offset-0 col-12 col-md-10">
+          <nav class="navbar__content">
+            <section class="title">
+              <nuxt-link exact class="navlink" to="/">Landlord Lookup</nuxt-link>
+            </section>
+            <section class="links">
+              <g-link exact class="navlink" to="/">Home</g-link>
+              <g-link exact class="navlink" to="/about">About</g-link>
+            </section>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.navbar {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  background: aliceblue;
+  box-shadow: 1px 1px 2px #aaa;
+  &__content {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .title {
+      .navlink {
+        font-size: 1.5rem;
+        font-weight: 700;
+        text-decoration: none;
+        color: black;
+      }
+    }
+    .links {
+      .navlink {
+        font-size: 1.125rem;
+      }
+    }
+  }
+}
+</style>
